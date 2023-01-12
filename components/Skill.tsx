454d36ea9c738890a44961ab139736c0701f2441
Kevin Torres <kevin.torres@kushkipagos.com>
2023-01-12 15:29:45 -0500

    fix bug on skills hover

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 type Props = {
   directionLeft?: boolean;
@@ -6,13 +7,18 @@ type Props = {
 
 const Skill = ({ directionLeft }: Props) => {
   return (
-    <div className="group relative flex cursor-pointer">
-      <motion.img
-        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
-        transition={{ duration: 1 }}
-        whileInView={{ opacity: 1, x: 0 }}
+    <motion.div
+      initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      className="group relative flex cursor-pointer"
+    >
+      <Image
         className="rounded-full border border-gray-500 object-cover w-24 h-24   md:w-28 md:h-28  filter group-hover:grayscale transition duration-300 ease-in-out"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png"
+        alt="tech-name"
+        width={112}
+        height={112}
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 rounded-full md:w-28 md:h-28 z-0 ">
         <div className="flex items-center justify-center h-full">
@@ -21,7 +27,7 @@ const Skill = ({ directionLeft }: Props) => {
           </p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
@@ -28,11 +29,14 @@ export default function Home() {
       <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
-      {/* Skills */}
+
       <section id="skills" className="snap-start">
         <Skills />
       </section>
       {/* Projects */}
+      <section id="skills" className="snap-start">
+        <Projects />
+      </section>
       {/* Contact me */}
     </div>
   );
